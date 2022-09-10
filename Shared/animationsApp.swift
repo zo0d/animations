827a1e57fa1e177a8eb368dc3680cedb7310453b
zo0d <bjahanzad@gmail.com>
2022-09-10 19:20:07 -0400

    Initial Commit

---
+++ Shared/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  Shared
+//
+//  Created by Behzad Jahanzad on 2022-09-10.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
